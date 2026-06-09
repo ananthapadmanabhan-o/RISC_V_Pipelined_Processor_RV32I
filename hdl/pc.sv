9bd68ac049de8ab4005101a81ca2417ab5902570
ananthapadmanabhan-o <ananthan51ah@gmail.com>
2026-06-09 15:52:56 +0530

    added program counter

---
+++ hdl/pc.sv
@@ -0,0 +1,16 @@
+module pc (
+	input logic clk,
+	input logic rst,
+	input logic [31:0] pc_in,
+	
+	output logic [31:0] pc_out
+);
+
+always_ff @(posedge clk) begin
+	if (rst)
+		pc_out <= 32'd0;
+	else
+		pc_out <= pc_in;
+end
+
+endmodule
