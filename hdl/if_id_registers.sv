f0a26fb23f03110e119cb541f4560154fa98766e
ananthapadmanabhan-o <ananthan51ah@gmail.com>
2026-06-10 12:38:59 +0530

    added stage 1 pipeline registers

---
+++ hdl/if_id_registers.sv
@@ -0,0 +1,28 @@
+module if_id_reg (
+	input logic clk,
+	input logic rst,
+	
+	input logic [31:0] pc,
+	input logic [31:0] pc_4,
+	input logic [31:0] instr,
+	
+	output logic [31:0] if_id_pc,
+	output logic [31:0] if_id_pc_4,
+	output logic [31:0] if_id_instr
+);
+
+always_ff @(posedge clk)
+	if (rst) begin
+		if_id_pc 	<= 32'd0;
+		if_id_pc_4 	<= 32'd0;
+		if_id_instr <= 32'd0;
+	end	
+	else begin
+		if_id_pc 	<= pc;
+		if_id_pc_4 	<= pc_4;
+		if_id_instr <= instr;
+	end
+end
+
+
+endmodule
