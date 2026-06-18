ee54f6186bd06541b26554e069b101ec79eb0fa3
ananthapadmanabhan-o <ananthan51ah@gmail.com>
2026-06-18 16:16:28 +0530

    added source register address to idex stage

--- hdl/id_ex_reg.sv
+++ hdl/id_ex_reg.sv
@@ -5,6 +5,9 @@ module id_ex_reg (
 	// operand data
 	input logic [31:0] rs1_data,
 	input logic [31:0] rs2_data,
+	//operand address
+	input logic [4:0] rs1,
+	input logic [4:0] rs2,
 
 	// immediate generator
 	input logic [31:0] imm,
@@ -30,23 +33,27 @@ module id_ex_reg (
 	
 	output logic id_ex_mem_read,
 	output logic id_ex_mem_write,
-	output logic id_ex_mem_to_reg
+	output logic id_ex_mem_to_reg,
+	output logic [4:0] id_ex_rs1,
+	output logic [4:0] id_ex_rs2
 );
 
 
 
 always_ff @(posedge clk) begin
 	if (rst) begin
-		id_ex_rs1_data 	<= '0;
-		id_ex_rs2_data 	<= '0;
-		id_ex_imm	 	<= '0;
-		id_ex_rd 		<= '0;
-		id_ex_reg_write <= '0;
-		id_ex_alu_src 	<= '0;
-		id_ex_alu_op  	<= '0;
-		id_ex_mem_read  <= '0;
-        id_ex_mem_write <= '0;
-        id_ex_mem_to_reg <= '0;
+		id_ex_rs1_data 	  <= '0;
+		id_ex_rs2_data 	  <= '0;
+		id_ex_imm	 	  <= '0;
+		id_ex_rd 		  <= '0;
+		id_ex_reg_write   <= '0;
+		id_ex_alu_src 	  <= '0;
+		id_ex_alu_op  	  <= '0;
+		id_ex_mem_read    <= '0;
+        id_ex_mem_write   <= '0;
+        id_ex_mem_to_reg  <= '0;
+        id_ex_rs1         <= '0;
+        id_ex_rs2         <= '0;
 		
 	end
 	else begin
@@ -61,6 +68,8 @@ always_ff @(posedge clk) begin
 		id_ex_mem_read  <= mem_read;
         id_ex_mem_write <= mem_write;
         id_ex_mem_to_reg <= mem_to_reg;
+        id_ex_rs1       <= rs1;
+        id_ex_rs2       <= rs2;
 		
 	end
 end
